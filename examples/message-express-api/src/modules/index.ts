d98a4abed5179c15219765d56fcf2c2512d9953c
noemi2312 <noemi.fernandez@uap.edu.ar>
2025-11-30 17:14:29 -0300

    nuevo proyecto

--- examples/message-express-api/src/modules/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./message/message.service";
-export * from "./wall/wall.repository";
-export * from "./wall/wall.service";
--- examples/message-react-jotai/src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Mensaje = {
-  id: string;
-  content: string;
-  likes: number;
-  createdAt: string;
-};
